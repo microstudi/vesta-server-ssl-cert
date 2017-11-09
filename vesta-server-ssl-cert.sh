6e393104413e8004a34759b65c2275c7cbd6720c
Ivan Vergés <ivan@microstudi.net>
2017-11-09 22:01:20 +0100

    change to Ubuntu 16.04

--- vesta-server-ssl-cert.sh
+++ vesta-server-ssl-cert.sh
@@ -4,51 +4,47 @@
 #
 # The script checks for new LetsEncrypt certificates in /home/admin/conf/web/ssl.[SERVER-FQDN].*
 # and it installs the new certificates for all services before restarting them.
+#
+# This script works for Ubuntu 16.04
 
 # Notification email parameters
+hostname=$(hostname -f)
+# If you want to force the hostname (my case):
+# hostname=yourhostname.tld
 mailto='CHANGE THIS TO YOUR EMAIL ADDRESS'
-mailsub="Server SSL Renewal: "$(hostname -f)
+mailsub="Server SSL Renewal: ${hostname}"
 
 # Set the paths of SSL certificates to check
 path2le=/home/admin/conf/web
 path2ve=/usr/local/vesta/ssl
 
 # Certificates to check
-LEcrt="${path2le}/ssl."$(hostname -f)".crt"
-LEkey="${path2le}/ssl."$(hostname -f)".key"
-LEpem="${path2le}/ssl."$(hostname -f)".pem"
+LEcrt="${path2le}/ssl.${hostname}.crt"
+LEkey="${path2le}/ssl.${hostname}.key"
+LEca="${path2le}/ssl.${hostname}.ca"
 VEcrt="${path2ve}/certificate.crt"
 VEkey="${path2ve}/certificate.key"
-VEpem="${path2ve}/certificate.pem"
+temp=$(tempfile)
+cat $LEcrt > $temp
+cat $LEca >> $temp
 
 # Compare current certificate with auto generated ones from LetsEncrypt
-if ! cmp --silent $LEcrt $VEcrt
+if ! cmp --silent $temp $VEcrt
 then
 	echo CERTIFICATES DIFFERENT - UPDATING
 	# Copy certificates
-	cp --backup $LEcrt $VEcrt
+	cp --backup $temp $VEcrt
 	cp --backup $LEkey $VEkey
-	cp --backup $LEpem $VEpem
 
 	# Set correct owner and permissions for certificates
 	chown root:mail $VEcrt $VEkey
-	chmod 660 $VEcrt $VEkey
+	chmod 640 $VEcrt $VEkey
 
-	# Restart services that depend on these certificates
-	case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
-		Debian)
-			type="debian" ;;
-		Ubuntu)
-			/usr/sbin/service vesta restart
-			/usr/sbin/service exim4 restart
-			/usr/bin/doveadm reload
-			/sbin/initctl restart vsftpd
-			;;
-		*)
-			systemctl restart vesta exim dovecot vsftpd
-			;;
-	esac
-	
+	systemctl restart vesta exim4 dovecot vsftpd
+
 	# Notify
-	which mail > /dev/null 2>&1 && echo "The server certificate at "$(hostname -f)" has been renewed successfully :)" | mail -s "$mailsub" "$mailto"
+	which mail > /dev/null 2>&1 && echo "The server certificate at "$(hostname -f)" has been r
+enewed successfully :)" | mail -s "$mailsub" "$mailto"
 fi
+
+rm $temp
